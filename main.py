67dce8e5af50fd3bd711e43357247e1c3cc7eb84
KDumpala <krishna71873@gmail.com>
2024-08-26 15:29:41 +0000

    init commit of 100

---
+++ main.py
@@ -0,0 +1,91 @@
+# band name generator
+
+# print("welcome to the band name generator \n")
+# city = input("enter the city you were born\n")
+# pet = input("enter your pet name \n")
+# print("your band name is\n"+city +" "+pet)
+
+####################################
+#Add two numbers
+
+# two_digit_number = input("Type a two digit number: ")
+# print(type(two_digit_number))
+# two_digit_number1= int(two_digit_number[0])
+# two_digit_number2= int(two_digit_number[1])
+# two_digit_number3= two_digit_number1+two_digit_number2
+# print(two_digit_number3)
+
+####################################
+# BMI calculator
+
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+
+# bmi= float(weight) / float(height)**2
+# print(int(bmi))
+
+####################################
+# age in weeks
+
+# age = input("input your current age")
+
+# days = round((90-float(age)) * 365)
+# weeks = round((90-float(age)) * 52)
+# months = round((90-float(age)) * 12)
+# print(f"you have {days} days, {weeks} weeks, and {months} months left")
+
+####################################
+# Tip calculator 
+
+# print("welcome to the tip calculator.")
+# total = float(input("What was the total bill."))
+# tip = float(input("What percentage tip you would like to give?."))
+# people =int(input("how many people to split the bill?."))
+# bill_pp = ((total/100 ) * tip + total)/ people
+# print(f"each person should pay: {bill_pp}")
+
+####################################
+#  odd-even
+
+# number = int(input("Which number do you want to check? "))
+# if number % 2 == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+####################################
+
+#  bmi calculator
+
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+
+# bmi = round(float(weight) / float(height)**2)
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi>18.5 and bmi<25:
+#     print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi>25 and bmi<30:
+#    print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi>30 and bmi<35:
+#     print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese.")
+
+####################################
+# Leap year.
+
+# year = int(input("Which year do you want to check? "))
+
+# if year % 4 == 0:
+#     if year % 100 ==0:
+#         if year % 400 ==0:
+#             print("Leap year")
+#         else:
+#             print("Not Leap year.")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not Leap year.")
+
+Test 
